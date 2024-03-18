d147dfe550a7139a8cb1b7ebc5919a4981b1503d
Johnny O'Donnell <odjohnny@amazon.com>
2024-03-18 15:21:00 -0700

    Edit comments

--- host-discovery-enumerator.sh
+++ host-discovery-enumerator.sh
@@ -3,15 +3,19 @@
 HOSTS=$1
 
 
-### Simple ping ###############################################################
+### Ping scans ################################################################
 
 sudo nmap -sn $HOSTS # Ping scan (ARP ping)
 nmap -sn $HOSTS # Ping scan (TCP Connect)
 sudo nmap -sn -PE -PP -PM $HOSTS # Ping scan (Timestamp and address mask)
 
-sudo nmap -Pn $HOSTS # No ping, top ports
-sudo nmap -Pn -sA -p- $HOSTS # No ping, TCP ACK, top ports
+### No ping, top ports ########################################################
 
-sudo nmap -Pn -p- $HOSTS # No ping, all ports
-sudo nmap -Pn -sA -p- $HOSTS # No ping, TCP ACK, all ports
+sudo nmap -Pn $HOSTS
+sudo nmap -Pn -sA -p- $HOSTS # TCP ACK
+
+### No ping, all ports ########################################################
+
+sudo nmap -Pn -p- $HOSTS
+sudo nmap -Pn -sA -p- $HOSTS # TCP ACK
 
