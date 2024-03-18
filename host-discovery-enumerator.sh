e24ba3528eba88b613800772783b30f1371880c5
Johnny O'Donnell <odjohnny@amazon.com>
2024-03-18 16:05:04 -0700

    Add special class of scans

--- host-discovery-enumerator.sh
+++ host-discovery-enumerator.sh
@@ -5,17 +5,25 @@ TARGET=$1
 
 ### Ping scans ################################################################
 
-sudo nmap -sn $TARGET # Ping scan (ARP ping)
+sudo nmap -sn -PE -PP -PM $TARGET # Ping scan (ARP ping, ICMP Timestamp, Address Mask)
+sudo nmap -sn -PA443 -PA80 $TARGET # Ping scan (TCP ACK Ping ports 80 + 443)
 nmap -sn $TARGET # Ping scan (TCP Connect)
-sudo nmap -sn -PE -PP -PM $TARGET # Ping scan (Timestamp and address mask)
 
-### No ping, top ports ########################################################
+### No ping, standard scan ####################################################
 
-sudo nmap -Pn $TARGET
+sudo nmap -Pn $TARGET # top ports
+sudo nmap -Pn -p- $TARGET # all ports
+
+### No ping, special scan, top ports ##########################################
+
+sudo nmap -Pn -sA $TARGET # TCP ACK
+sudo nmap -Pn -sY $TARGET # SCTP INIT
+
+### No ping, special scan, all ports ##########################################
+
 sudo nmap -Pn -sA -p- $TARGET # TCP ACK
+sudo nmap -Pn -sY -p- $TARGET # SCTP INIT
 
-### No ping, all ports ########################################################
 
-sudo nmap -Pn -p- $TARGET
-sudo nmap -Pn -sA -p- $TARGET # TCP ACK
+## Currently no UDP scans
 
