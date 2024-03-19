6d4c9f7e8dd7e7b308b2090833239e2675515ed2
Johnny O'Donnell <odjohnny@amazon.com>
2024-03-19 11:34:04 -0700

    Fix how arguments are passed to the subprocess

--- host-discovery-enumerator.py
+++ host-discovery-enumerator.py
@@ -12,73 +12,73 @@ commands = [
         {
             "name": "Web Ports",
             "commands": [
-                "-Pn -p 80,443 --open 172.17.64.82/20",
+                "-Pn -p 80,443 --open",
                 ],
             },
         {
             "name": "TCP SYN Scan, Top 10 ports",
             "commands": [
-                "-Pn --top-ports 10 --open 172.17.64.82/20",
+                "-Pn --top-ports 10 --open",
                 ],
             },
         {
             "name": "TCP SYN Scan, Top 100 ports",
             "commands": [
-                "-Pn --top-ports 100 --open 172.17.64.82/20",
+                "-Pn --top-ports 100 --open",
                 ],
             },
         {
             "name": "TCP ACK Scan, Top 10 ports",
             "commands": [
-                "-sA -Pn --top-ports 10 --open 172.17.64.82/20",
+                "-sA -Pn --top-ports 10 --open",
                 ],
             },
         {
             "name": "TCP ACK Scan, Top 100 ports",
             "commands": [
-                "-sA -Pn --top-ports 100 --open 172.17.64.82/20",
+                "-sA -Pn --top-ports 100 --open",
                 ],
             },
         {
             "name": "TCP Window Scan, Top 10 ports",
             "commands": [
-                "-sW -Pn --top-ports 10 --open 172.17.64.82/20",
+                "-sW -Pn --top-ports 10 --open",
                 ],
             },
         {
             "name": "Special TCP Scans, Top 10 ports",
             "commands": [
-                "-sN -Pn --top-ports 10 --open 172.17.64.82/20",
-                "-sF -Pn --top-ports 10 --open 172.17.64.82/20",
-                "-sX -Pn --top-ports 10 --open 172.17.64.82/20",
-                "-sM -Pn --top-ports 10 --open 172.17.64.82/20",
+                "-sN -Pn --top-ports 10 --open",
+                "-sF -Pn --top-ports 10 --open",
+                "-sX -Pn --top-ports 10 --open",
+                "-sM -Pn --top-ports 10 --open",
                 ],
             },
         {
             "name": "TCP SYN Scan, All ports",
             "commands": [
-                "-Pn -p- --open 172.17.64.82/20",
+                "-Pn -p- --open",
                 ],
             },
         {
             "name": "TCP Ack Scan, All ports",
             "commands": [
-                "-sA -Pn -p- --open 172.17.64.82/20",
+                "-sA -Pn -p- --open",
                 ],
             },
         {
             "name": "TCP Window Scan, All ports",
             "commands": [
-                "-sW -Pn -p- --open 172.17.64.82/20",
+                "-sW -Pn -p- --open",
                 ],
             },
         {
             "name": "Special Scans, All ports",
             "commands": [
-                "-sN -Pn -p- --open 172.17.64.82/20",
-                "-sF -Pn -p- --open 172.17.64.82/20",
-                "-sX -Pn -p- --open 172.17.64.82/20",
-                "-sM -Pn -p- --open 172.17.64.82/20",
+                "-sN -Pn -p- --open",
+                "-sF -Pn -p- --open",
+                "-sX -Pn -p- --open",
+                "-sM -Pn -p- --open",
                 ],
             },
         ]
@@ -98,8 +98,11 @@ def run(target, index = 1):
         command_set = commands[i]
         print("-- Now running: " + get_full_name(i + 1, command_set["name"]) + "---")
         for command in command_set["commands"]:
-            subprocess.run(["sudo", "nmap", target])
-
+            subprocess_args = ["sudo", "nmap"]
+            subprocess_args.extend(command.split(" "))
+            subprocess_args.append(target)
+            print(" ".join(subprocess_args))
+            subprocess.run(subprocess_args)
 
 def main():
     print_commands()
