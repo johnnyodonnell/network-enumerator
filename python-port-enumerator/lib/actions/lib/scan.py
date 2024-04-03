93ca40491fac918c3a834db43a6c3c9c0d78a47e
Johnny O'Donnell <odjohnny@amazon.com>
2024-04-03 10:03:02 -0700

    Read output in separate thread

--- python-port-enumerator/lib/actions/lib/scan.py
+++ python-port-enumerator/lib/actions/lib/scan.py
@@ -1,12 +1,44 @@
 import os
 import re
 import subprocess
+import threading
+import time
 import xml.etree.ElementTree as ET
+
+from threading import Thread
 
 from lib.state import save_state
 
 
+# From https://stackoverflow.com/a/325528
+class ReadOutputThread(threading.Thread):
+    """Thread class with a stop() method. The thread itself has to check
+    regularly for the stopped() condition."""
+
+    def __init__(self, output_filename, current_state):
+        super(ReadOutputThread, self).__init__()
+        self.output_filename = output_filename
+        self.current_state = current_state
+        self._stop_event = threading.Event()
+
+    def stop(self):
+        self._stop_event.set()
+
+    def stopped(self):
+        return self._stop_event.is_set()
+
+    def run(self):
+        while not self.stopped():
+            if os.path.isfile(self.output_filename):
+                copy_output_to_state(self.output_filename, self.current_state)
+            time.sleep(5)
+        copy_output_to_state(self.output_filename, self.current_state)
+
+
 def run_scan(args, target, output_filename, current_state):
+    read_output = ReadOutputThread(output_filename, current_state)
+    read_output.start()
+
     command = [
             "sudo", "nmap", "-v",
             "--max-scan-delay", "5ms", "--max-retries", "1",
@@ -19,7 +51,8 @@ def run_scan(args, target, output_filename, current_state):
     else:
         command.append(target)
     subprocess.run(command)
-    copy_output_to_state(output_filename, current_state)
+    read_output.stop()
+    read_output.join()
 
 def process_host(current_stage, host_map, host):
     address = host.find("address").get("addr")
